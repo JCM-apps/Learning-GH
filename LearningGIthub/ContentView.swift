53ce8f20649915a05682bde6ea1954d8de6f0085
JCM <JCM>
2023-02-07 14:30:10 -0600

    Initial Commit

---
+++ LearningGIthub/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearningGIthub
+//
+//  Created by Juan Meneses on 2/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGIthub/LearningGIthubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGIthubApp.swift
+//  LearningGIthub
+//
+//  Created by Juan Meneses on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGIthubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
